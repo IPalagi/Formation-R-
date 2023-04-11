8e4430f8ef51c81eb426586931e645699df88954
IPalagi <imane.palagi@gmail.com>
2023-04-11 10:30:52 +0200

    modif

---
+++ Formation.R
@@ -0,0 +1,5 @@
+#### Notes #### 
+
+# package Rposgresql pour inteeroger des bases sql sans avoir à charger les données sur R 
+# en tydiverse on utilise pas le $ pour sélectionner les colonne, on fait pool
+
